e752d03bdf4b4e14b24f4d4737bdcec5ed8f9809
Oksana <oksana.korobko.fd@gmail.com>
2023-10-09 15:13:54 +0300

    Create object Movies

--- films.js
+++ films.js
@@ -23,3 +23,10 @@
 
 'use strict';
 let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?");
+let personalMovieDB ={
+    count:numberOfFilms,
+    movies : {},
+    actors : {},
+    genres : [],
+    privat : false,
+}
